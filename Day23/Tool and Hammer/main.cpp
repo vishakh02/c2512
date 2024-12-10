711e382494dfb1261180587a6b290f429323ab47
vishakh02 <110244118+vishakh02@users.noreply.github.com>
2024-12-10 13:58:08 +0530

    Add files via upload

---
+++ Day23/Tool and Hammer/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// Base class: Tool
+class Tool {
+protected:
+    char* toolName;
+    double weight;
+
+public:
+    // Constructor
+    Tool(const char* name, double wgt) {
+        toolName = new char[strlen(name) + 1];
+        strcpy(toolName, name);
+        weight = wgt;
+        cout << "Tool constructor called.." << endl;
+    }
+
+    // Destructor
+    virtual ~Tool() {
+        cout << "Tool destructor called.." << endl;
+        delete[] toolName;
+    }
+
+    // Pure virtual behaviors
+    virtual void use() = 0;
+    virtual void repair() = 0;
+};
+
+// Derived class: Hammer
+class Hammer : public Tool {
+protected:
+    char* handleMaterial;
+
+public:
+    // Constructor
+    Hammer(const char* name, double wgt, const char* material)
+        : Tool(name, wgt) {
+        handleMaterial = new char[strlen(material) + 1];
+        strcpy(handleMaterial, material);
+        cout << "Hammer constructor called.." << endl;
+    }
+
+    // Destructor
+    ~Hammer() {
+        cout << "Hammer destructor called.." << endl;
+        delete[] handleMaterial;
+    }
+
+    // Override behaviors
+    void use() override {
+        cout << "Using the hammer: " << toolName 
+             << " weighing " << weight << "kg, with a handle made of "
+             << handleMaterial << "." << endl;
+    }
+
+    void repair() override {
+        cout << "Repairing with the hammer: " << toolName 
+             << " weighing " << weight << "kg, with a handle made of "
+             << handleMaterial << "." << endl;
+    }
+};
+
+int main() {
+    // Create dynamic objects of Hammer pointed by Tool pointers
+    Tool* tool1 = new Hammer("Claw Hammer", 1.5, "Wood");
+    Tool* tool2 = new Hammer("Sledge Hammer", 4.0, "Steel");
+
+    cout << "=== Tool 1 ===" << endl;
+    tool1->use();
+    tool1->repair();
+
+    cout << "=== Tool 2 ===" << endl;
+    tool2->use();
+    tool2->repair();
+
+    // Cleanup
+    delete tool1;
+    delete tool2;
+
+    return 0;
+}
