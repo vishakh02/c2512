52e7f046c203250ce53444c1e215697769948f43
vishakh02 <110244118+vishakh02@users.noreply.github.com>
2024-12-17 10:46:46 +0530

    Update program01.cpp

--- Evaluation03/program01.cpp
+++ Evaluation03/program01.cpp
@@ -1,57 +1,72 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-class Employee{
-    private:
-     int Id;
-     double age;
-     string name;
+class Employee {
+private:
+    int id;        
+    int age;       
+    string name;   
 
-    public:
-     void swap(Employee& other);
-     Employee(int Id, double age, string name);
-     
-     friend ostream& operator<<(ostream& output, const Employee& Employee);
+public:
+    // Parameterized Constructor
+    Employee(int empId, int empAge, const string& empName) 
+        : id(empId), age(empAge), name(empName) {
+            
+            cout << "Default Constructor" <<endl;
+        }
+
+    // Move Constructor
+    Employee(Employee&& other) noexcept
+        : id(other.id), age(other.age), name(std::move(other.name)) {
+        cout << "\n" << "Move Constructor" << endl;
+        other.id = 0;
+        other.age = 0;
+    }
+
+    // Move Assignment Operator
+    Employee& operator=(Employee&& other) noexcept {
+        if (this != &other) {
+            
+            id = other.id;
+            age = other.age;
+            name = std::move(other.name);
+            
+            other.id = 0;
+            other.age = 0;
+        }
+        return *this;
+    }
+
+    // Overload << operator for displaying Employee details
+    friend ostream& operator<<(ostream& output, const Employee& emp) {
+        output << emp.id << " " << emp.age << " " << emp.name;
+        return output;
+    }
+
+    
+    void swap(Employee& other) {
+        
+        Employee temp = std::move(*this);  
+        *this = std::move(other);          
+         other = std::move(temp);           
+    }
 };
 
-ostream& operator<<(ostream& output, const Employee& employee){
-    output << employee.Id << ", " << employee.age << ", " << employee.name;
-    return output;
-}
+int main() {
+    // Creating Employee objects
+    Employee e1(101, 22, "Athira");
+    Employee e2(102, 23, "Bhagya");
 
-int main(){
-    Employee e1(101,22,"Athira");
-    Employee e2(102,23,"Bhagya");
-
-    cout << "\n"; cout << "Original values: "<< endl; cout << "\n";
-    cout << e1 << endl;
-    cout << e2 << endl;
+    cout << "\n"; cout << "Before swapping:" << endl;
+    cout << e1 << endl; // 101 22 Athira
+    cout << e2 << endl; // 102 23 Bhagya
 
     e1.swap(e2);
-
-    cout << "\n"; cout << "Swapped values: "<< endl; cout << "\n";
-    cout << e1 << endl;
-    cout << e2 << endl;
+    
+     cout << "\n"; cout << "After swapping:" << endl;
+    cout << e1 << endl; // 102 23 Bhagya
+    cout << e2 << endl; // 101 22 Athira
 
     return 0;
 }
-
-void Employee::swap(Employee& other){
-    int temp = this->Id;
-    this->Id = other.Id;
-    other.Id = temp;
-
-    double temp2 = this->age;
-    this->age = other.age;
-    other.age = temp2;
-
-    string temp3 = this->name;
-    this->name = other.name;
-    other.name = temp3;
-}
-
-Employee::Employee(int Id, double age, string name): Id(Id), age(age), name(name){
-        cout << "Constructer invoked" << endl;
-}
-
