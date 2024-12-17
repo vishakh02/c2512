eaaf5541f2792999382d975756db1f287ffeb403
vishakh02 <110244118+vishakh02@users.noreply.github.com>
2024-12-17 12:09:52 +0530

    Update program03.cpp

--- Evaluation03/program03.cpp
+++ Evaluation03/program03.cpp
@@ -1,125 +1,180 @@
+//1.3 Programmer { string* tasks; int taskCount; } : Employee | tasks is the multiple tasks - array of task 
+        // swap function overloadable
+        // here, create the dynamic programmer object assigned to employee pointer.
+        // field 'tasks' is dynamic memoery.
+        // operator << overloadable, swap is overridden.
+
+
 #include <iostream>
 #include <cstring>
+#include <utility>
 
 using namespace std;
 
-
-class Employee {
-protected:
-    int* Id;
-    double* age;
-    char* name;
-
-public:
-    virtual void swap(Employee& other);
-    Employee(int Id, double age, const char* name);
-    virtual ~Employee();
+// Employee class
+class Employee 
+{
+    private:
+        int* id;
+        int* age;     
+        char* name;
     
-    friend ostream& operator<<(ostream& output, const Employee& employee);
+    public:
+        // Constructor
+        Employee(int id, int age, const char* name) 
+        {
+            cout << "\n" << "Argument Constructor"<<endl;
+            this->id = new int(id);                  
+            this->age = new int(age);   
+            this->name = new char[strlen(name) + 1]; 
+            strcpy(this->name, name);
+        }
+    
+        // Destructor
+        virtual ~Employee() 
+        {
+            cout << "Destructor called"<<endl;
+            delete id;   
+            delete age;  
+            delete[] name; 
+        }
+    
+        // Move Constructor
+        Employee(Employee&& other) noexcept 
+            : id(other.id), age(other.age), name(other.name) 
+        {
+            cout << "\n" << "MoveConstructor"<<endl;
+            other.id = nullptr;
+            other.age = nullptr;
+            other.name = nullptr; 
+        }
+    
+        // Move Assignment Operator
+        Employee& operator=(Employee&& other) noexcept
+        {
+            if (this != &other) 
+            {
+                delete id;
+                delete age;
+                delete[] name;
+    
+                id = other.id;
+                age = other.age;
+                name = other.name;
+    
+                other.id = nullptr;
+                other.age = nullptr;
+                other.name = nullptr;
+            }
+            return *this;
+        }
+    
+        virtual void swap(Employee& other) 
+        {
+            Employee temp(std::move(other)); 
+            other = std::move(*this);       
+            *this = std::move(temp);    
+        }
+    
+        friend ostream& operator<<(ostream& out, const Employee& employee);
 };
 
-class Programmer : public Employee {
-private:
-    string* tasks;
-    int taskCount;
+ostream& operator<<(ostream& out, const Employee& employee)
+{
+    out << *employee.id << " " << *employee.age << " " << employee.name;
+    return out;
+}
+
+class Programmer : public Employee 
+{
+    private:
+        string* tasks; 
+        int taskCount; 
     
-public:
-    void swap(Programmer& other) ;
-    Programmer(int Id, double age, const char* name, string* tasks, int taskCount);
-    ~Programmer();
+    public:
+        // Constructor
+        Programmer(int id, int age, const char* name, int taskCount, const string* tasks)
+            : Employee(id, age, name), taskCount(taskCount) 
+        {
+            cout << "Programmer Arg Constructor"<<endl;
+            this->tasks = new string[taskCount];
+            for (int i = 0; i < taskCount; ++i) 
+            {
+                this->tasks[i] = tasks[i];
+            }
+        }
     
-    friend ostream& operator<<(ostream& output, const Programmer& programmer);
+        // Destructor
+        ~Programmer() 
+        {
+            cout << "\n" << "Programmer Destructorr"<<endl;
+            delete[] tasks; 
+        }
+    
+        // Move Constructor
+        Programmer(Programmer&& other) noexcept
+            : Employee(std::move(other)), taskCount(other.taskCount), tasks(other.tasks) 
+        {
+            cout << "Programmer MoveConstructor"<<endl;
+            other.tasks = nullptr; 
+        }
+    
+        // Move Assignment Operator
+        Programmer& operator=(Programmer&& other) noexcept 
+        {
+            if (this != &other) 
+            {
+                delete[] tasks;
+    
+                Employee::operator=(std::move(other));
+    
+                tasks = other.tasks;
+                
+                taskCount = other.taskCount;
+    
+                other.tasks = nullptr;
+            }
+            return *this;
+        }
+    
+        // Swap function using Move Constructor
+        void swap(Programmer& other) 
+        {
+            Programmer temp(std::move(other)); // Move other into temp
+            other = std::move(*this);          // Move this into other
+            *this = std::move(temp);           // Move temp back into this
+        }
+    
+        friend ostream& operator<<(ostream& os, const Programmer& prog);
 };
 
-int main() {
-    
-    string tasks1[] = {"Task01", "Task02", "Task03"};
-    Employee* p1 = new Programmer(101, 22, "Athira", tasks1, 3);
-    
-    string tasks2[] = {"Task0A", "Task0B", "Task0C"};
-    Employee* p2 = new Programmer(102, 23, "Bhagya", tasks2, 3);
+ostream& operator<<(ostream& out, const Programmer& programmer) 
+{
+    out << static_cast<const Employee&>(programmer) << " Tasks: ";
+    for (int i = 0; i < programmer.taskCount; ++i)
+    {
+        out << programmer.tasks[i] << (i < programmer.taskCount - 1 ? ", " : "");
+    }
+    return out;
+}
 
-    cout << "\nOriginal values: " << endl;
-    cout << *p1 << endl;
-    cout << *p2 << endl;
+int main() 
+{
+    const string tasks1[] = {"Task01", "Task02", "Task03"};
+    Programmer p1(101, 23, "Athira", 3, tasks1);
 
-    p1->swap(*p2);
+    const string tasks2[] = {"Task_A", "Task_B", "Task_C"};
+    Programmer p2(102, 22, "Bhagya", 3, tasks2);
 
-    cout << "\nSwapped values: " << endl;
-    cout << *p1 << endl;
-    cout << *p2 << endl;
-    cout << "\n";
+    cout << " \n Before swapping:" << endl;
+    cout << p1 << endl; 
+    cout << p2 << endl; 
 
-    delete p1;
-    delete p2;
+    p1.swap(p2);
+
+    cout << "\nAfter swapping:" << endl;
+    cout << p1 << endl; 
+    cout << p2 << endl; 
 
     return 0;
-}
-
-// Employee class methods
-void Employee::swap(Employee& other) {
-    int tempId = *this->Id;
-    *this->Id = *other.Id;
-    *other.Id = tempId;
-
-    double tempAge = *this->age;
-    *this->age = *other.age;
-    *other.age = tempAge;
-
-    char* tempName = this->name;  
-    this->name = other.name;   
-    other.name = tempName;       
-}
-
-Employee::Employee(int Id, double age, const char* name) {
-    cout << "Employee Constructor Invoked" << endl;
-    
-    this->Id = new int(Id);
-    this->age = new double(age);
-    this->name = new char[strlen(name) + 1];
-    strcpy(this->name, name);
-}
-
-Employee::~Employee() {
-    cout << "Employee Destructor Invoked" << endl;
-    delete Id;
-    delete age;
-    delete[] name;
-}
-
-ostream& operator<<(ostream& output, const Employee& employee) {
-    output << *employee.Id << ", " << *employee.age << ", " << employee.name;
-    return output;
-}
-
-// Programmer class methods
-void Programmer::swap(Programmer& other) {
-    Employee::swap(static_cast<Employee&>(other));  // Use base class swap directly
-    
-    string* tempTasks = this->tasks;
-    this->tasks = other.tasks;
-    other.tasks = tempTasks;
-}
-
-Programmer::Programmer(int Id, double age, const char* name, string* tasks, int taskCount)
-    : Employee(Id, age, name), taskCount(taskCount) {
-    cout << "Programmer Constructor called" << endl;
-    this->tasks = new string[taskCount];
-    for (int i = 0; i < taskCount; ++i) {
-        this->tasks[i] = tasks[i];
-    }
-}
-
-Programmer::~Programmer() {
-    cout << "Programmer Destructor invoked" << endl;
-    delete[] tasks;
-}
-
-ostream& operator<<(ostream& output, const Programmer& programmer) {
-    output << static_cast<const Employee&>(programmer);  // Use base class operator<< directly
-    output << "Tasks: ";
-    for (int i = 0; i < programmer.taskCount; ++i) {
-        output << programmer.tasks[i] << ", ";
-    }
-    cout << endl;
 }
