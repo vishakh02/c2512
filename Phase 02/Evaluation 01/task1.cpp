03f677e0470d8a2443c554ddb01a2720956b0543
vishakh02 <110244118+vishakh02@users.noreply.github.com>
2025-01-16 20:00:24 +0530

    Create task1.cpp

---
+++ Phase 02/Evaluation 01/task1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+class Patient {
+private:
+    string PatientId;
+    int PatientAge;
+
+public:
+    Patient(string id, int age) : PatientId(id), PatientAge(age) {}
+
+    string getPatientId() const {
+        return PatientId;
+    }
+
+    int getPatientAge() const {
+        return PatientAge;
+    }
+};
+
+int findSum(const vector<Patient>& patients) {
+    int sum = 0;
+    for (const auto& each : patients) {
+        sum += each.getPatientAge();
+    }
+    return sum;
+}
+
+void findMinInFirstHalf(const vector<Patient>& patients, int& min) {
+    min = INT_MAX;
+    for (int i = 0; i < patients.size() / 2; ++i) {
+        if (patients[i].getPatientAge() < min) {
+            min = patients[i].getPatientAge();
+        }
+    }
+}
+
+void findMaxInSecondHalf(const vector<Patient>& patients, int& max) {
+    max = INT_MIN;
+    for (int i =((patients.size() / 2) + 1); i < patients.size(); ++i) {
+        if (patients[i].getPatientAge() > max) {
+            max = patients[i].getPatientAge();
+        }
+    }
+}
+
+int main() {
+    vector<Patient> patients = {
+        Patient("101", 1),
+        Patient("102", 2),
+        Patient("103", 3),
+        Patient("104", 4),
+        Patient("105", 5)
+    };
+
+    // Calling functions
+    int sum = findSum(patients);
+    cout << "The sum is " << sum << endl;
+
+    int min;
+    findMinInFirstHalf(patients, min);
+    cout << "The min in first half is " << min << endl;
+
+    int max;
+    findMaxInSecondHalf(patients, max);
+    cout << "The max in second half is " << max << endl;
+
+    return 0;
+}
